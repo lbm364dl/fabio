ad54d8725216357f100386b78d559e0604d6f68e
lbm364dl <geometrydashm364@gmail.com>
2025-03-10 11:56:10 +0100

    Use single prep script

--- R/00_prep.R
+++ R/00_prep.R
@@ -1,12 +1,59 @@
-test <- function(destfile, url) {
-  if (!file.exists(destfile)) {
-    print(dirname(destfile))
-    destfile |>
-      dirname() |>
-      dir.create(recursive = TRUE)
-    path <- download.file(url, destfile, timeout = )
-    print(path)
+download <- function(url, destdir, alias, extension) {
+  destfile <- stringr::str_glue("{destdir}/{alias}.{extension}")
+  print("destfile")
+  print(destfile)
+  if (file.exists(destfile)) {
+    return(destfile)
   }
+  dir.create(destdir, recursive = TRUE)
+
+  path <- tryCatch(
+    download.file(url, destfile, mode = "wb", method = "curl"),
+    error = function(cond) {
+      if (file.exists(destfile)) {
+        file.remove(destfile)
+      }
+      stop("File was not downloaded correctly. Try again.")
+    }
+  )
+  print(path)
+  path
+}
+
+# Select codes for fish (30___) and ethanol (2207__)
+filter_fish_and_ethanol <- function(destfile, ...) {
+  baci_sel <- data.table::fread(destfile)
+  baci_sel[grep("^(30[1-5]..|2207..)", baci_sel$k), ]
+}
+
+save_baci_rds <- function(input_files, k_path_trade, k_baci_group) {
+  baci_rds_path <- paste0(k_path_trade, "baci_sel.rds")
+  if (file.exists(baci_rds_path)) {
+    return(baci_rds_path)
+  }
+  input_files |>
+    dplyr::filter(group == k_baci_group) |>
+    purrr::pmap(filter_fish_and_ethanol) |>
+    data.table::rbindlist() |>
+    saveRDS(baci_rds_path)
+}
+
+save_rds <- function(destfile, destdir, alias, extension, ...) {
+  rds_destfile <- stringr::str_glue("{destdir}/{alias}.rds")
+  if (file.exists(rds_destfile)) {
+    return(rds_destfile)
+  }
+
+  table <-
+    if (extension == "xlsx") {
+      destfile |>
+        openxlsx::read.xlsx() |>
+        data.table::as.data.table()
+    } else {
+      data.table::fread(destfile)
+    }
+
+  saveRDS(table, rds_destfile, compress = FALSE)
 }
 
 options(timeout = 1e6)
@@ -15,4 +62,13 @@ input_files <-
   "inst/input_files.csv" |>
   readr::read_csv() |>
   dplyr::rowwise() |>
-  dplyr::mutate(df = test(destfile, url))
+  dplyr::mutate(destfile = download(url, destdir, alias, extension))
+
+k_path_trade <- "input/trade/"
+
+k_baci_group <- "baci"
+save_baci_rds(input_files, k_path_trade, k_baci_group)
+
+input_files |>
+  dplyr::filter(group != k_baci_group) |>
+  purrr::pmap(save_rds)
--- R/main.R
+++ R/main.R
@@ -1,22 +1,25 @@
 
 # 0_prep ------------------------------------------------------------------
 
-library("data.table") # 1.12.0
-library("comtradr") # 0.2.2
+# library("data.table") # 1.12.0
+# library("comtradr") # 0.2.2
 # library("openxlsx") # 4.1.0
 
-source("R/00_prep_functions.R")
+# source("R/00_prep_functions.R")
 
 # Will download required ZIP files and convert the contents to RDS
-source("R/00_prep_fao.R")
+# source("R/00_prep_fao.R")
 
 # Requires EIA and IEA CSV files that are available from:
 # https://www.eia.gov/opendata/qb.php?category=2135203 (in 1000 bbl/d)
 # http://dx.doi.org/10.1787/data-00550-en
-source("R/00_prep_eth.R")
+# source("R/00_prep_eth.R")
 
 # Requires BACI92 ZIP files as well as data.table::rbindlist(comtrade)
-source("R/00_prep_trade.R")
+# source("R/00_prep_trade.R")
+
+# New prep script
+source("R/00_prep.R")
 
 # Depends on outputs produced in step 0
 source("R/01_tidy_fao.R")
