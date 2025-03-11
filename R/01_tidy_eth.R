fe61845eac3ad217cc25bb20e77f0bb23d1dd690
lbm364dl <geometrydashm364@gmail.com>
2025-03-11 10:05:08 +0100

    Do not use unavailable iea data

--- R/01_tidy_eth.R
+++ R/01_tidy_eth.R
@@ -41,35 +41,39 @@ rm(eth_eia_extr)
 
 # IEA Bio-Ethanol ---------------------------------------------------------
 
-cat("\nTidying IEA ethanol.\n")
+# cat("\nTidying IEA ethanol.\n")
 
-eth_iea <- readRDS("input/ethanol/eth_iea.rds")
+# eth_iea <- readRDS("input/ethanol/eth_iea.rds")
 
-# Country concordance
-country_match <- match(eth_iea[["country"]], regions[["iso3c"]])
-eth_iea[, `:=`(area = regions$name[country_match],
-  area_code = regions$code[country_match], country = NULL)]
+# # Country concordance
+# country_match <- match(eth_iea[["country"]], regions[["iso3c"]])
+# eth_iea[, `:=`(area = regions$name[country_match],
+#   area_code = regions$code[country_match], country = NULL)]
 
-eth_iea <- dt_filter(eth_iea, !is.na(area))
+# eth_iea <- dt_filter(eth_iea, !is.na(area))
 
-eth_iea <- melt(eth_iea, id.vars = c("area", "area_code"),
-  variable.name = "year", value.name = "value_iea", variable.factor = FALSE)
+# eth_iea <- melt(eth_iea, id.vars = c("area", "area_code"),
+#   variable.name = "year", value.name = "value_iea", variable.factor = FALSE)
 
-cat("Converting from ktoe to tonnes",
-    "(1 ktoe == 919.09 tonnes).\n")
-eth_iea <- dt_filter(eth_iea, !is.na(value_iea))
-eth_iea[, `:=`(value_iea = round(value_iea * 919.09, 3), unit = "tonnes")]
+# cat("Converting from ktoe to tonnes",
+#     "(1 ktoe == 919.09 tonnes).\n")
+# eth_iea <- dt_filter(eth_iea, !is.na(value_iea))
+# eth_iea[, `:=`(value_iea = round(value_iea * 919.09, 3), unit = "tonnes")]
 
-rm(country_match)
+# rm(country_match)
 
 
 # Bio-Ethanol -------------------------------------------------------------
+# Only data from EIA because I have not found the one from IEA
+# (http://dx.doi.org/10.1787/data-00550-en seems to require payment)
 
-eth <- merge(eth_eia, eth_iea, all = TRUE)
+# eth <- merge(eth_eia, eth_iea, all = TRUE)
+eth_eia[, `:=`(value = value_eia, value_eia = NULL)]
+eth <- eth_eia
 
-cat("Merging EIA and IEA ethanol - values chosen via `max(eia, iea)`.\n")
-eth[, `:=`(value = pmax(value_eia, value_iea, na.rm = TRUE),
-  value_eia = NULL, value_iea = NULL, year = as.integer(year))]
+# cat("Merging EIA and IEA ethanol - values chosen via `max(eia, iea)`.\n")
+# eth[, `:=`(value = pmax(value_eia, value_iea, na.rm = TRUE),
+#   value_eia = NULL, value_iea = NULL, year = as.integer(year))]
 
 eth <- area_merge(eth, orig = 62, dest = 238, pattern = "Ethiopia")
 eth <- area_merge(eth, orig = 206, dest = 276, pattern = "Sudan")
@@ -78,4 +82,5 @@ eth <- area_fix(eth, regions)
 
 # Store
 saveRDS(eth, "data/tidy/eth_tidy.rds")
-rm(eth, eth_eia, eth_iea)
+# rm(eth, eth_eia, eth_iea)
+rm(eth, eth_eia)
